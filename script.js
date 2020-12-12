5c78c137d25b740f83a3443dceb1ed52cfe6e8e1
mokulling <72229978+mokulling@users.noreply.github.com>
2020-12-12 16:31:49 -0600

    retrieve input

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 var queryUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='
 var apiKey = '&appid=27d5701c8ffececd64c7ed1f0876ec1a'
-var city = 'Chicago'
-var cityInputId = $('#citysearch')
+var city = ''
+var cityInputId = $('#searchbox')
 var cityName= $('#city-name')
 var cityTemp= $('#city-temp')
 var humidity= $('#humidity')
 var windSpeed= $('#windSpeed')
 var uv = $('#uv')
+var cityInput = cityInputId.val()
 
+
+
+$('input').on('search',(function(event) {
+        event.preventDefault();
+        var value = $(this).val();
+        console.log(value);
+        city=value
+        
+}))
 
 
 $.ajax({
     url: queryUrl+ city +apiKey,
     method: 'GET',
-}).then(function(response){
-    console.log(response)
-    var cityNameOut = (response.city.name)
-    var cityTempOut= response.list[0].main.temp
-    var cityHumidityOut = response.list[0].main.humidity
-    var cityWindOut = response.list[0].wind.speed
     
-   cityName.append(cityNameOut)
-    cityTemp.append(cityTempOut)
-    humidity.append(cityHumidityOut)
-    windSpeed.append(cityWindOut)
-
-})
+    }).then(function(response){
+        console.log(response)
+        var cityNameOut = (response.city.name)
+        var cityTempOut= response.list[0].main.temp
+        var cityHumidityOut = response.list[0].main.humidity
+        var cityWindOut = response.list[0].wind.speed
+    
+        cityName.append(cityNameOut)
+        cityTemp.append(cityTempOut)
+        humidity.append(cityHumidityOut)
+        windSpeed.append(cityWindOut)
+    })
+      
+    
