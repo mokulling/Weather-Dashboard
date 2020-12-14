8645249067a29854aeee5ace2b8dd3cd5900add7
mokulling <72229978+mokulling@users.noreply.github.com>
2020-12-14 17:11:49 -0600

    added future temps to forecast

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ var uv = $('#uv')
 var cityInput = cityInputId.val()
 var newcitystore = $('#newcity')
 var newCity = ''
+var dayOneDate = $('#date-1')
+var dayOneTemp = $('#temp-1')
+var dayOneHum = $('#hum-1')
+
 
 //search button listener
 
@@ -21,17 +25,17 @@ var newCity = ''
         console.log(value);
         newcitystore.append('<p>' + value)
         city=value
-    
        
     
     }))  
 
   
-//console.log(form)
+
 
 
 
 //ajax call to weather api
+
 $.ajax({
     url: queryUrl+ city +apiKey,
     method: 'GET',
@@ -43,10 +47,14 @@ $.ajax({
         var cityTempOut= response.list[0].main.temp
         var cityHumidityOut = response.list[0].main.humidity
         var cityWindOut = response.list[0].wind.speed
+        var cityTempTwoOut = response.list[1].main.temp
+        var cityHumTwoOut = response.list[1].main.humidity
     
         cityName.append(cityNameOut)
         cityTemp.append(cityTempOut)
         humidityDiv.append(cityHumidityOut)
         windSpeedDiv.append(cityWindOut)
+        dayOneTemp.append(cityTempTwoOut)
+        dayOneHum.append(cityHumTwoOut)
     })
  
